be099c5f46ee06ffe2c1902abc62e63826dd83d5
yashi-1111 <89307679+yashi-1111@users.noreply.github.com>
2022-04-29 09:41:56 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA5ZFyuf2XvJfZStviCHdgq0yCYFL6rWoQ",
+    authDomain: "p-94-50ae2.firebaseapp.com",
+    projectId: "p-94-50ae2",
+    storageBucket: "p-94-50ae2.appspot.com",
+    messagingSenderId: "372059693102",
+    appId: "1:372059693102:web:97a7fc376002b5c5077fbf"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
